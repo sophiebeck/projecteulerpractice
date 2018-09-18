9b4ef35db299946b451156ee60552e0545fbb00a
Sophie <sophiebeck661@gmail.com>
2018-09-18 08:56:09 -0400

    made first problem

--- JavaEuler.java
+++ JavaEuler.java
@@ -2,4 +2,9 @@ public class JavaEuler{
 	public static void main(String[]args){
 
 	}
+	//takes in 2 multiples and a number to end on
+	//returns sum of the multiples below specified int
+	public static int sumMultiplesBelowInt(int firstMult, int secondMult, int finalNum){
+		
+	}
 }
